e46de598d250078637d90f23c559f32c3df0ef3b
Torbots User <info@team1197.com>
2020-01-06 19:12:20 -0800

    Changed some imports and set pom.xml

--- 2020InfiniteRecharge/src/main/java/frc/robot/Robot.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/Robot.java
@@ -1,6 +1,6 @@
 package frc.robot;
 
-// import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
@@ -9,7 +9,7 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.Joystick;
 
-// import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorSensorV3;
 
 public class Robot extends TimedRobot {
   private Joystick player1;
@@ -24,8 +24,8 @@ public class Robot extends TimedRobot {
   private TalonSRX rightSlave1;
   private TalonSRX rightSlave2;
   
-  // private final I2C.Port i2cPort = I2C.Port.kOnboard;
-  // private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
   public Robot() {
     player1 = new Joystick(0);
@@ -91,18 +91,58 @@ public class Robot extends TimedRobot {
     leftMaster.set(ControlMode.PercentOutput, leftMotorSpeed);
     rightMaster.set(ControlMode.PercentOutput, rightMotorSpeed);
 
-    // Color detectedColor = m_colorSensor.getColor();
+    //Color Sensor Code
+    
+    Color detectedColor = m_colorSensor.getColor();
 
-    // double IR = m_colorSensor.getIR();
+    //double IR = m_colorSensor.getIR();
+    
+    // Cyan Green Red Yellow
+    //R 0.132 0.173 0.500 0.315
+    //G 0.426 0.563 0.352 0.554
+    //B 0.441 0.263 0.147 0.132
+    
+    String ColorDetected = "None";
+    // double Rmax = 0.0;
+    // double Gmax = 0.0;
+    // double Bmax = 0.0;
+    // if(Rmax < detectedColor.red) {
+    //   Rmax = detectedColor.red;
+    // }
+    // if(Gmax < detectedColor.green) {
+    //   Gmax = detectedColor.green;
+    // }
+    // if(Bmax < detectedColor.blue) {
+    //   Bmax = detectedColor.blue;
+    // }
 
-    // SmartDashboard.putNumber("Red", detectedColor.red);
-    // SmartDashboard.putNumber("Green", detectedColor.green);
-    // SmartDashboard.putNumber("Blue", detectedColor.blue);
-    // SmartDashboard.putNumber("IR", IR);
-
-    // int proximity = m_colorSensor.getProximity();
-
-    // SmartDashboard.putNumber("Proximity", proximity);
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    //SmartDashboard.putNumber("Red Max", Rmax);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    //SmartDashboard.putNumber("Green Max", Gmax);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    //SmartDashboard.putNumber("Blue Max", Bmax);
+    
+    if(detectedColor.red > detectedColor.blue && detectedColor.red > detectedColor.green && detectedColor.red > 0.4){
+      ColorDetected = "Red";
+    } else if(Math.abs(detectedColor.red-detectedColor.green) < 0.25) {
+      ColorDetected = "Yellow";
+    } else if(Math.abs(detectedColor.green-detectedColor.blue) < 0.25) {
+      ColorDetected = "Blue";
+    } else if(detectedColor.green > detectedColor.blue && detectedColor.green > detectedColor.red) {
+      ColorDetected = "Green"; 
+    } else {
+      ColorDetected = "Invalid";
+    }
+    int proximity = m_colorSensor.getProximity();
+    if(proximity<50) {
+      ColorDetected = "Out Of Range";
+    }
+    SmartDashboard.putString("Color Detected", ColorDetected);
+    
+    //SmartDashboard.putNumber("IR", IR);
+    
+    //SmartDashboard.putNumber("Proximity", proximity);
   }
 
   @Override
