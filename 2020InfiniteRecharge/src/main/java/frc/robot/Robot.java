9b8b40e354e6c481ef432bcb4031d264ee55ac2c
Torbots User <info@team1197.com>
2020-01-11 21:43:42 -0800

    Working on distance calc

--- 2020InfiniteRecharge/src/main/java/frc/robot/Robot.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/Robot.java
@@ -3,6 +3,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -12,17 +13,28 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
   private final double kP = 0.03;
-  private TalonSRX turretMotor;
-  private NetworkTable table;//for limelight
-  private NetworkTableEntry tx;
-  // private NetworkTableEntry ta;
+  private final TalonSRX turretMotor;
+  private final VictorSPX hoodMotor;
+  private final NetworkTable table;// for limelight
+  private final NetworkTableEntry tx;
+  private final NetworkTableEntry ty;
+  //private final NetworkTableEntry ta;
   private double x;
+  private double y;
+  //private double a;
+  private double h1;
+  private double h2;
+  private double a1;
+  private double a2;
+  private double d;
 
   public Robot() {
     turretMotor = new TalonSRX(7);
+    hoodMotor = new VictorSPX(8);
     table = NetworkTableInstance.getDefault().getTable("limelight-ball");
     tx = table.getEntry("tx");
-    // ta = table.getEntry("ta");
+    ty = table.getEntry("ty");
+    //ta = table.getEntry("ta");
   }
   @Override
   public void robotInit() {
@@ -45,6 +57,18 @@ public class Robot extends TimedRobot {
     x = tx.getDouble(0.0);
     SmartDashboard.putNumber("X:",x);
     turretMotor.set(ControlMode.PercentOutput, kP * x);
+    y = ty.getDouble(0.0);
+    SmartDashboard.putNumber("Y:",y);
+    a1 = 15;
+    a2 = y;
+    h1 = 41;
+    h2= 94.5;
+    d = (h2-h1) / Math.tan(Math.toRadians(a1+a2));
+    SmartDashboard.putNumber("D", d);
+    hoodMotor.set(ControlMode.PercentOutput, kP * d);
+
+
+
   }
 
   @Override
