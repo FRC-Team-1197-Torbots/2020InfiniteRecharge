e7dd140191530fd5c000068c32107bbbed156456
Torbots User <info@team1197.com>
2020-01-12 16:29:40 -0800

    Doing better

--- 2020InfiniteRecharge/src/main/java/frc/robot/Robot.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/Robot.java
@@ -15,25 +15,10 @@ public class Robot extends TimedRobot {
   private final double kP = 0.03;
   private final TalonSRX turretMotor;
   private final VictorSPX hoodMotor;
-  private final NetworkTable table;// for limelight
-  private final NetworkTableEntry tx;
-  private final NetworkTableEntry ty;
-  //private final NetworkTableEntry ta;
-  private double x;
-  private double y;
-  //private double a;
-  private double h1;
-  private double h2;
-  private double a1;
-  private double a2;
-  private double d;
-
+  
   public Robot() {
     turretMotor = new TalonSRX(7);
     hoodMotor = new VictorSPX(8);
-    table = NetworkTableInstance.getDefault().getTable("limelight-ball");
-    tx = table.getEntry("tx");
-    ty = table.getEntry("ty");
     //ta = table.getEntry("ta");
   }
   @Override
@@ -54,20 +39,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    x = tx.getDouble(0.0);
-    SmartDashboard.putNumber("X:",x);
-    turretMotor.set(ControlMode.PercentOutput, kP * x);
-    y = ty.getDouble(0.0);
-    SmartDashboard.putNumber("Y:",y);
-    a1 = 15;
-    a2 = y;
-    h1 = 41;
-    h2= 94.5;
-    d = (h2-h1) / Math.tan(Math.toRadians(a1+a2));
-    SmartDashboard.putNumber("D", d);
-    hoodMotor.set(ControlMode.PercentOutput, kP * d);
-
-
+    
+    
 
   }
 
---
+++ 2020InfiniteRecharge/src/main/java/frc/robot/Turret.java
@@ -0,0 +1,112 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
+public class Turret {
+    private final double dt = 0.005;
+    private final TalonSRX turretMotor;
+    private final VictorSPX hoodMotor;
+
+    private final NetworkTable table;// for limelight
+    private final NetworkTableEntry tx;
+    private final NetworkTableEntry ty;
+    //private final NetworkTableEntry ta;
+    private double x;
+    private double y;
+
+    //PID for the horizontal spinner of the turret
+    private final double hozkP = 0.03;
+    private final double hozkI = 0;
+    private final double hozkD = 0;
+    private TorDerivative horizontalDerivative;
+    private double horizontalVelocity;
+    private double horizontalIntegral = 0;
+    //PID for the hood angle adjustment
+    private final double hoodkP = 0;
+    private final double hoodkI = 0;
+    private final double hoodkD = 0;
+    private final double hoodGearRatio = 500;//from the encoder to the movement fo the hood
+    private double currentHoodAngle;
+    private double currentHoodError;
+
+    private final double height1 = 31;
+    private final double height2 = 94.5;
+    private final double angle1 = 15;
+    private double angle2;
+    private double distance;
+    private double intermediateCalculation;
+    private double hoodAngleToSet;
+
+    private double horizontalSpeedToSet;
+    private double hoodSpeedToSet;
+
+    public Turret(TalonSRX turretMotor, VictorSPX hoodMotor) {
+        table = NetworkTableInstance.getDefault().getTable("limelight-ball");
+        tx = table.getEntry("tx");
+        ty = table.getEntry("ty");
+        this.turretMotor = turretMotor;
+        this.hoodMotor = hoodMotor;
+
+        hoodMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+        hoodMotor.setSelectedSensorPosition(0, 0, 0);
+
+        horizontalDerivative = new TorDerivative(dt);
+        horizontalDerivative.resetValue(0);
+    }
+
+    public void run(boolean run) {//gets angle of the hood
+        x = tx.getDouble(0.0);
+        SmartDashboard.putNumber("X:",x);
+        horizontalSpeedToSet = hozPID(x);
+        if(run) {
+            turretMotor.set(ControlMode.PercentOutput, horizontalSpeedToSet);
+        }
+
+
+        y = ty.getDouble(0.0);
+        SmartDashboard.putNumber("Y:",y);
+        angle2 = y;
+        distance = (height2-height1) * (1 / Math.tan(Math.toRadians(angle1+angle2)));
+        SmartDashboard.putNumber("D", distance);
+        intermediateCalculation = ((98.25 - height1) / distance);
+        hoodAngleToSet = Math.atan(intermediateCalculation + (15/distance));//hood angle is radians
+        hoodAngleToSet *= 180 / (Math.PI);//hood angle is now degrees
+
+        SmartDashboard.putNumber("Intermediate Calculation" , intermediateCalculation);
+        SmartDashboard.putNumber("Hood Angle" , hoodAngleToSet);   
+        
+        
+    }
+
+    public double hoodPID(double hoodAngleToSet) {
+        currentHoodAngle = 40 - 
+            (360 * (hoodMotor.getSelectedSensorPosition(0) / (4096 * hoodGearRatio)));
+        currentHoodError = hoodAngleToSet - currentHoodAngle;
+        //everything goes off of current hood error
+
+        return 1;
+    }
+
+    public double hozPID(double currentAngle) {
+        horizontalVelocity = horizontalDerivative.estimate(currentAngle);
+        horizontalIntegral += currentAngle * dt;
+        if(currentAngle < 0.25) {
+            horizontalIntegral = 0;
+        }
+        if(horizontalIntegral * hozkI > 0.5) {
+            horizontalIntegral = 0.5 / hozkI;
+        } else if(horizontalIntegral * hozkI < -0.5) {
+            horizontalIntegral = -0.5 / hozkI;
+        }
+        return ((currentAngle * hozkP) + 
+            (horizontalVelocity * hozkD) + 
+            (horizontalIntegral * hozkI));
+    }
+}
